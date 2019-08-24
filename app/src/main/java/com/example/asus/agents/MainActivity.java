a678f125d09e8372cfbfb4fff44a53457636d27d
RickyRiko305 <pranaymunda.pm@gmail.com>
2019-08-24 23:48:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/asus/agents/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.asus.agents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/asus/agents/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package com.example.asus.agents;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                try{
+                    sleep(3000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                finally {
+                    Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                }
+            }
+        };
+        thread.start();
+    }
+}
