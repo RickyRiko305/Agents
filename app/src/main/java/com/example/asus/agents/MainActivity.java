cd43cc8c2b4892319f0957451670150e9bb509cc
RickyRiko305 <pranaymunda.pm@gmail.com>
2019-08-25 22:33:28 +0530

    start page activity

--- app/src/main/java/com/example/asus/agents/MainActivity.java
+++ app/src/main/java/com/example/asus/agents/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.asus.agents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser == null){
+            Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+            startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(startIntent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/asus/agents/WelcomeActivity.java
+++ app/src/main/java/com/example/asus/agents/WelcomeActivity.java
@@ -30,4 +30,12 @@ public class WelcomeActivity extends AppCompatActivity {
         };
         thread.start();
     }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        finish();
+    }
 }
